0e1d2d6a6f4f8c308a351fd088254822a406eee3
_ wilvurson _ <wilvurson@gmail.com>
2026-02-06 15:38:40 +0800

    added

--- app/navbar/navber.tsx
+++ app/navbar/navber.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, UserAvatar } from "@clerk/nextjs";
+import { UserCircle } from "lucide-react"
 import Link from "next/link";
 
 export default function Navbar() {
   return (
     <header className="fixed top-0 left-0 w-full bg-white shadow z-50 h-auto">
-      <div className="max-w-6xl flex p-4 gap-x-5">
-        <UserButton />
+      <div className="w-auto flex p-4 gap-x-5 justify-around">
+        <Link href="/admin">
+              <UserCircle className="h-10 w-8 text-stone-500"/>
+          </Link>
         <div className="flex gap-x-2 w-120">
           <Link href="/home">
             <div className="text-black bg-stone-200 rounded-3xl h-auto">
@@ -37,21 +40,12 @@ export default function Navbar() {
               </div>
             </div>
           </Link>
-          <Link href="/admin">
-            <div className="text-black bg-stone-200 rounded-3xl h-auto">
-              <div className="h-10 flex flex-col justify-center px-10">
-                Admin
-              </div>
-            </div>
-          </Link>
-          <Link href="/profile">
-            <div className="text-black bg-stone-200 rounded-3xl h-auto">
-              <div className="h-10 flex flex-col justify-center px-10">
-                Profile
-              </div>
-            </div>
-          </Link>
         </div>
+        <Link href="/profile">
+            <div className="text-black bg-stone-200 rounded-3xl h-10 w-10 flex items-center justify-center">
+              <UserAvatar />
+            </div>
+          </Link>
       </div>
     </header>
   );

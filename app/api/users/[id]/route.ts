3ea83bc024c1e84888c5dd5881eee91d1a816265
Bat-orgil421 <batorgildorjbat@gmail.com>
2026-02-23 15:59:04 +0800

    insd

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "../../../../lib/prisma";
+import { prisma } from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 
 export async function PUT(
